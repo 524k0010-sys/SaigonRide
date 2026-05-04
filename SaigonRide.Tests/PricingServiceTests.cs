d417cc8eed9d4e931ef154b7e36935d98ce988a7
524k0010-sys <524k0010@student.tdtu.edu.vn>
2026-05-05 04:17:35 +0700

    add pricing discount boundary tests

---
+++ SaigonRide.Tests/PricingServiceTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SaigonRide.Models;
+using SaigonRide.Services;
+
+namespace SaigonRide.Tests
+{
+    [TestClass]
+    public class PricingServiceTests
+    {
+        [TestMethod]
+        public void CalculateBaseFare_StandardBike10Minutes_Returns5000()
+        {
+            var service = new PricingService();
+            var start = new DateTime(2026, 5, 5, 10, 0, 0);
+            var end = new DateTime(2026, 5, 5, 10, 10, 0);
+
+            var result = service.CalculateBaseFare(start, end, 500m);
+
+            Assert.AreEqual(5000m, result);
+        }
+
+        [TestMethod]
+        public void CalculateBaseFare_EScooter10Minutes_Returns15000()
+        {
+            var service = new PricingService();
+            var start = new DateTime(2026, 5, 5, 10, 0, 0);
+            var end = new DateTime(2026, 5, 5, 10, 10, 0);
+
+            var result = service.CalculateBaseFare(start, end, 1500m);
+
+            Assert.AreEqual(15000m, result);
+        }
+
+        [TestMethod]
+        public void CalculateDiscount_Inventory19Percent_Returns15PercentDiscount()
+        {
+            var service = new PricingService();
+            var station = new Station
+            {
+                Capacity = 100,
+                CurrentInventory = 19
+            };
+
+            var discount = service.CalculateDiscount(100000m, station);
+
+            Assert.AreEqual(15000m, discount);
+        }
+
+        [TestMethod]
+        public void CalculateDiscount_Inventory20Percent_ReturnsNoDiscount()
+        {
+            var service = new PricingService();
+            var station = new Station
+            {
+                Capacity = 100,
+                CurrentInventory = 20
+            };
+
+            var discount = service.CalculateDiscount(100000m, station);
+
+            Assert.AreEqual(0m, discount);
+        }
+
+        [TestMethod]
+        public void CalculateDiscount_Inventory21Percent_ReturnsNoDiscount()
+        {
+            var service = new PricingService();
+            var station = new Station
+            {
+                Capacity = 100,
+                CurrentInventory = 21
+            };
+
+            var discount = service.CalculateDiscount(100000m, station);
+
+            Assert.AreEqual(0m, discount);
+        }
+    }
+}
---
+++ SaigonRide.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SaigonRide.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SaigonRide.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("44a4d520-e29c-4e93-89a3-ba59fffbae98")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
